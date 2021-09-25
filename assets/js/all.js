5b065a4030902c68a880f7b566a9fef23939f8e0
圈圈 <vvvvvvii@yahoo.com.tw>
2021-09-25 22:29:32 +0800

    Update 2021-09-25T14:29:28.704Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -19,19 +19,40 @@ var productCategory = '握壽司';
 var storeCategory = '尋找最近的店舖'; // 渲染畫面
 
 function render() {
-  if (document.querySelector('#product-list')) {
+  if (document.querySelector('#productList')) {
     // 產品列表頁
+    // 先確定網址有無參數
+    var url = location.href.split('?type=');
+
+    if (url[1]) {
+      productCategory = decodeURI(url[1]);
+    } // 抓 api
+
+
     products.where('category', '==', productCategory).orderBy('price').onSnapshot(function (querySnapshot) {
       var str = '';
       querySnapshot.forEach(function (item) {
         var product = item.data();
-        str += "\n        <div class=\"col-4\">\n          <div class=\"card border-0\">\n            <img src=\"".concat(product.imgUrl, "\" alt=\"").concat(product.title, "\" class=\"card-img\">\n            <div class=\"card-body\">\n              <h3 class=\"card-title mb-0\">").concat(product.title, "</h3>\n              <p class=\"text-center font-2\">").concat(product.price, " \u5143</p>\n            </div>\n          </div>\n        </div>\n        ");
-        document.querySelector('#product-list').innerHTML = str;
+        str += "\n        <div class=\"col-md-4 col-sm-6 col-12 mb-7\">\n          <div class=\"card border-0\">\n            <img src=\"".concat(product.imgUrl, "\" alt=\"").concat(product.title, "\" class=\"card-img\">\n            <div class=\"card-body\">\n              <h3 class=\"card-title mb-0\">").concat(product.title, "</h3>\n              <p class=\"text-center font-2\">").concat(product.price, " \u5143</p>\n            </div>\n          </div>\n        </div>\n        ");
+      });
+      $('#productList').html(str);
+      $('#productListTitle').html(productCategory); // 第一次進入頁面時， nav-item 即可正確顯示 active class
+
+      document.querySelectorAll('.nav-link').forEach(function (item) {
+        if (item.innerHTML === productCategory) {
+          item.parentElement.classList.add('active');
+        }
       });
     });
-  } else if (document.querySelector('#store-list')) {
+  } else if (document.querySelector('#storeList')) {
     // 店舖列表頁
     var storeArr = [];
+
+    var _url = location.href.split('?type=');
+
+    if (_url[1]) {
+      storeCategory = decodeURI(_url[1]);
+    }
 
     if (storeCategory === '尋找最近的店舖') {
       stores.onSnapshot(function (querySnapshot) {
@@ -64,33 +85,49 @@ function render() {
 function renderStoreList(storeArr) {
   var str = '';
   storeArr.forEach(function (store) {
-    str += "\n    <div class=\"col-lg-4 mb-6\">\n      <div class=\"card\">\n        <img src=\"".concat(store.imgUrl, "\" alt=\"").concat(store.category).concat(store.title, "\u5E97\" class=\"card-img\">\n        <div class=\"card-body\">\n          <h3 class=\"card-title\">").concat(store.category).concat(store.title, "\u5E97</h3>\n          <ul>\n            <li class=\"mb-2\">\n              <i class=\"bi bi-telephone-fill\"></i>\n              <span class=\"ml-1\">").concat(store.tel, "</span>\n            </li>\n            <li class=\"mb-2\">\n              <i class=\"bi bi-clock-fill\"></i>\n              <span class=\"ml-1\">").concat(store.time[0], ":00 ~ ").concat(store.time[1], ":00</span>\n            </li>\n            <li>\n              <i class=\"bi bi-pin-angle-fill\"></i>\n              <span class=\"ml-1\">").concat(store.address, "</span>\n            </li>\n          </ul>\n        </div>\n        <div class=\"card-footer\">\n          <a href=\"#\" class=\"btn btn-secondary-light\">\u7ACB\u5373\u8A02\u4F4D</a>\n          <a href=\"#\" class=\"btn btn-outline-secondary-light ml-2\">\u5916\u9001\u9EDE\u9910</a>\n        </div>\n      </div>\n    </div>\n    ");
+    str += "\n    <div class=\"col-lg-4 col-md-6 col-12 mb-7\">\n      <div class=\"card\">\n        <img src=\"".concat(store.imgUrl, "\" alt=\"").concat(store.category).concat(store.title, "\u5E97\" class=\"card-img\">\n        <div class=\"card-body\">\n          <h3 class=\"card-title\">").concat(store.category).concat(store.title, "\u5E97</h3>\n          <ul>\n            <li class=\"mb-2\">\n              <i class=\"bi bi-telephone-fill\"></i>\n              <span class=\"ml-1\">").concat(store.tel, "</span>\n            </li>\n            <li class=\"mb-2\">\n              <i class=\"bi bi-clock-fill\"></i>\n              <span class=\"ml-1\">").concat(store.time[0], ":00 ~ ").concat(store.time[1], ":00</span>\n            </li>\n            <li class=\"d-flex\">\n              <i class=\"bi bi-pin-angle-fill\"></i>\n              <span class=\"ml-1\">").concat(store.address, "</span>\n            </li>\n          </ul>\n        </div>\n        <div class=\"card-footer\">\n          <a href=\"#\" class=\"btn btn-secondary-light\">\u7ACB\u5373\u8A02\u4F4D</a>\n          <a href=\"#\" class=\"btn btn-outline-secondary-light ml-2\">\u5916\u9001\u9EDE\u9910</a>\n        </div>\n      </div>\n    </div>\n    ");
   });
-  document.querySelector('#store-list').innerHTML = str;
+  $('#storeList').html(str);
 }
 
 $(document).ready(function () {
+  // 渲染畫面
   render(); // 漢堡選單
 
-  $('#collapse-navbar-btn').on('click', function () {
+  $('#collapseNavbarBtn').on('click', function () {
     $('#collapse-navbar-menu').slideToggle();
-  }); // 點選列表，被點到的加上 active class
+  }); // 點選列表，沒被點到的移除 active class (render function 中會加上對應 active class)
 
   $('.nav-link').on('click', function (event) {
     event.preventDefault();
-    $(this).parent().addClass('active');
     $(this).parent().siblings().removeClass('active'); // 如果是產品列表頁，要秀出對應資料
 
-    if (document.querySelector('#product-list')) {
-      $('#product-list-title').html($(this)[0].innerHTML);
+    if ($('#productList')) {
       productCategory = $(this)[0].innerHTML;
+      var url = "/product-list.html?type=".concat(productCategory); // 更新網址參數但不刷新整個頁面讓 nav-item active 消失
+
+      history.replaceState({
+        url: url,
+        title: document.title
+      }, document.title, url);
     }
 
     render();
-  }); // 點選店舖 select ，秀出對應資料
+  }); // 首頁搜尋店舖，跳轉畫面秀出對應資料
 
-  $('#search-store-btn').on('click', function () {
+  $('#searchStoreBtnIndex').on('click', function () {
     storeCategory = $(this).siblings()[0].value;
+    var url = "/store-location.html?type=".concat(storeCategory);
+    document.location = url;
+  }); // 店舖頁搜尋店舖，秀出對應資料
+
+  $('#searchStoreBtnLocation').on('click', function () {
+    storeCategory = $(this).siblings()[0].value;
+    var url = "/store-location.html?type=".concat(storeCategory);
+    history.replaceState({
+      url: url,
+      title: document.title
+    }, document.title, url);
     render();
   });
 });
